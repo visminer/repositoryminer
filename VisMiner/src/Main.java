08846b3ba8761c5c2404ccf9f355c20b15fc31d7
luanalima <luana.lima9@gmail.com>
2014-06-14 17:10:30 -0300

    modificação postgres

--- VisMiner/src/Main.java
+++ VisMiner/src/Main.java
@@ -1,3 +1,4 @@
+
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -6,6 +7,9 @@ import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.persistence.config.PersistenceUnitProperties;
 import org.visminer.git.remote.GitHub;
 import org.visminer.main.VisMiner;
+import org.visminer.persistence.ConfigConnectionPostgre;
+import org.visminer.persistence.ConfigConnectionMysql;
+import org.visminer.persistence.ConfigConnection;
 
 
 public class Main {
@@ -13,41 +17,35 @@ public class Main {
 
 	public static void main(String[] args) throws IOException, GitAPIException {
 
-		Map<String, String> props = new HashMap<String, String>(5);
-		props.put(PersistenceUnitProperties.JDBC_DRIVER, "com.mysql.jdbc.Driver");
+		  ConfigConnection config = new ConfigConnectionMysql();
+		  //ConfigConnection config = new ConfigConnectionPostgre();
+		   
+		   HashMap<String, String> props = config.sgbdConnection();
+		 
+		   Map<Integer, String> api_cfg_local = new HashMap<Integer, String>(3);
+			api_cfg_local.put(VisMiner.LOCAL_REPOSITORY_PATH,
+					"C:\\Users\\Luana\\Documents\\GitHub\\Visminer\\.git");
+			
+			api_cfg_local.put(VisMiner.LOCAL_REPOSITORY_NAME, "visminer");
+			api_cfg_local.put(VisMiner.LOCAL_REPOSITORY_OWNER, "luanalima");
+		   
 
-		props.put(PersistenceUnitProperties.JDBC_URL, "jdbc:mysql://localhost/visminer");
-		props.put(PersistenceUnitProperties.JDBC_USER, "");
-		props.put(PersistenceUnitProperties.JDBC_PASSWORD, ""); 	
-		props.put(PersistenceUnitProperties.DDL_GENERATION, "create-tables");
-
-		Map<Integer, String> api_cfg_local = new HashMap<Integer, String>(3);
-		api_cfg_local.put(VisMiner.LOCAL_REPOSITORY_PATH,
-				"/path/.git");
-		
-
-		api_cfg_local.put(VisMiner.LOCAL_REPOSITORY_NAME, "");
-		api_cfg_local.put(VisMiner.LOCAL_REPOSITORY_OWNER, "");
-		
-		/*
-		 * These isn't obligatory, all parameters of "api_cfg_remote" could be
-		 * set how null or could be don't set.
-		 */
-		Map<Integer, Object> api_cfg_remote = new HashMap<Integer, Object>(3);
-		api_cfg_remote.put(VisMiner.REMOTE_REPOSITORY_LOGIN, null);
-		api_cfg_remote.put(VisMiner.REMOTE_REPOSITORY_PASSWORD, null);
-		
-		/*
-		 * Object that implements the interface Connection, this object
-		 * is some remote repository that works with git.
-		 */
-		api_cfg_remote.put(VisMiner.REMOTE_REPOSITORY_GIT, new GitHub());
-				
-		VisMiner visminer = new VisMiner(props, api_cfg_local, api_cfg_remote);
-	
-		System.out.print(visminer.getMetric("NOP").getDescription());
-
-		
+		  /*
+		  for(Committer committer : visminer.getCommitters()){
+		   for(Commit commit : visminer.getCommits(committer)){
+		    if(visminer.getFiles(commit).size() > 0){
+		     System.out.println(visminer.getFiles(commit).get(0).getPath());
+		    }
+		   }
+		  } */
+			Map<Integer, Object> api_cfg_remote = new HashMap<Integer, Object>(3);
+			api_cfg_remote.put(VisMiner.REMOTE_REPOSITORY_LOGIN, null);
+			api_cfg_remote.put(VisMiner.REMOTE_REPOSITORY_PASSWORD, null);		
+			api_cfg_remote.put(VisMiner.REMOTE_REPOSITORY_GIT, new GitHub());
+			
+			VisMiner visminer = new VisMiner(props, api_cfg_local, api_cfg_remote);
+		  System.out.println(visminer.getMetrics().get(0).getDescription());
 	}
+		
 	
 }
