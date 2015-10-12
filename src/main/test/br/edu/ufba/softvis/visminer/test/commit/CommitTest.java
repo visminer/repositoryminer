b6fef0f07898e0f1648a43fbae210c0c42f06d15
Uilian Carvalho <uiliancarvalho02@gmail.com>
2015-10-12 17:36:54 -0300

    Tests on commits repository

--- src/main/test/br/edu/ufba/softvis/visminer/test/commit/CommitTest.java
+++ src/main/test/br/edu/ufba/softvis/visminer/test/commit/CommitTest.java
@@ -1,13 +1,75 @@
 package br.edu.ufba.softvis.visminer.test.commit;
 
 import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.util.List;
+
+import br.edu.ufba.softvis.visminer.main.VisMiner;
+import br.edu.ufba.softvis.visminer.model.business.Commit;
+import br.edu.ufba.softvis.visminer.model.business.Repository;
+import br.edu.ufba.softvis.visminer.test.VisminerTest;
 
 public class CommitTest {
 	
+	private static VisminerTest visminerTest;
+	private static VisMiner visminer;
+	private static Repository repository;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		
+		visminerTest = VisminerTest.getInstance();
+		visminer = visminerTest.getVisminer();
+		repository = visminerTest.getRepository(); 
+		
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testSizeOfWholeListOfCommits() {
+		List<Commit> commits = repository.getProject().getCommits();
+		assertEquals(103, commits.size());
 	}
 
+	@Test
+	public void testFirstCommit() {
+		repository.getProject().firstCommit();
+		Commit commit = repository.getProject().getCurrentCommit();
+		assertEquals(1, commit.getId());
+	}
+	
+	@Test
+	public void testLastCommit() {
+		repository.getProject().lastCommit();
+		Commit commit = repository.getProject().getCurrentCommit();
+		assertEquals(17, commit.getId());
+	}
+	
+	@Test 
+	public void testDateLastCommitDifferentDateFirstCommit() {
+		repository.getProject().firstCommit();
+		Commit firstCommit = repository.getProject().getCurrentCommit();
+		
+		repository.getProject().lastCommit();
+		Commit lastCommit = repository.getProject().getCurrentCommit();
+		
+		assertNotEquals(firstCommit.getDate(), lastCommit.getDate());
+	}
+	
+	@Test
+	public void testSecondCommitMoreFileCommitedFirstCommit() {
+		
+		Commit firstcommit = null;
+		Commit secondCommit = null;
+		
+		repository.getProject().firstCommit();
+		firstcommit = repository.getProject().getCurrentCommit();
+		
+		repository.getProject().nextCommit();
+		secondCommit = repository.getProject().getCurrentCommit();
+		
+		assertTrue(secondCommit.getCommitedFiles().size() > firstcommit.getCommitedFiles().size());
+	}
 }
