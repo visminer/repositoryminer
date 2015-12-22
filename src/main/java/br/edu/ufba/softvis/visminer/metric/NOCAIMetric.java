d53d0f7d345c74c40b21e4594805e8b444045da0
luispscarvalho <luispscarvalho@gmail.com>
2015-12-22 17:39:11 -0300

    removal of classes to ease the effort in migrating to MONGODB (VII)
    As for the refactoring of visminer to adopt a non-relational DB, I have
    reduced source-code items to minimum to lessen the number of "broken"
    files/classes. 
    
    They might return with future commits as other refactoring steps take
    place.

--- src/main/java/br/edu/ufba/softvis/visminer/metric/NOCAIMetric.java
+++ src/main/java/br/edu/ufba/softvis/visminer/metric/NOCAIMetric.java
@@ -1,65 +1,58 @@
 package br.edu.ufba.softvis.visminer.metric;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import org.bson.Document;
+
 import br.edu.ufba.softvis.visminer.annotations.MetricAnnotation;
 import br.edu.ufba.softvis.visminer.ast.AST;
-import br.edu.ufba.softvis.visminer.ast.Document;
-import br.edu.ufba.softvis.visminer.constant.MetricType;
-import br.edu.ufba.softvis.visminer.constant.MetricUid;
-import br.edu.ufba.softvis.visminer.model.business.Commit;
-import br.edu.ufba.softvis.visminer.persistence.MetricPersistance;
 
 @MetricAnnotation(
-		name = "Number of Classes and Interfaces",
-		description = "Number of Classes and Interfaces is a software metric used to measure the size of a computer program"+
-				" by counting the concrete and abstract classes",
-		acronym = "NOCAI",
-		type = MetricType.SNAPSHOT,
-		uid = MetricUid.NOCAI
-	)
-public class NOCAIMetric implements IMetric{
+	name = "Number of Classes and Interfaces", 
+	description = "Number of Classes and Interfaces is a software metric used to measure the size of a "
+			+ "computer program"
+		+ " by counting the concrete and abstract classes", 
+	acronym = "NOCAI")
+public class NOCAIMetric implements IMetric {
 
 	@Override
-	public void calculate(List<AST> astList, List<Commit> commits, MetricPersistance persistence){
-		
-		Map<Integer, Integer> packageCls = new HashMap<Integer, Integer>();
+	public void calculate(AST ast, Document document) {
+
+		Map<String, Integer> packageCls = new HashMap<String, Integer>();
 		int projectQtd = 0;
-		for(AST ast : astList){
-			
+		for (AST ast : astList) {
+
 			Document doc = ast.getDocument();
 			int id, num = 0;
 
-			if(doc.getTypes() != null){
+			if (doc.getTypes() != null) {
 				num += doc.getTypes().size();
 			}
-			
-			if(doc.getPackageDeclaration() != null){
-				
+
+			if (doc.getPackageDeclaration() != null) {
+
 				id = doc.getPackageDeclaration().getId();
-				
-				if(packageCls.containsKey(id)){
+
+				if (packageCls.containsKey(id)) {
 					int aux = packageCls.get(id);
-					packageCls.put(id, aux + num);
-				}else{
-					packageCls.put(id, num);
+					packageCls.put(id + "", aux + num);
+				} else {
+					packageCls.put(id + "", num);
 				}
-				
+
 			}
 
 			projectQtd += num;
-			
+
 		}
-		
-		for(Entry<Integer, Integer> entry : packageCls.entrySet()){
+
+		for (Entry<String, Integer> entry : packageCls.entrySet()) {
 			persistence.postMetricValue(entry.getKey(), String.valueOf(entry.getValue()));
 		}
-		
-		persistence.postMetricValue(persistence.getProject().getId(), String.valueOf(projectQtd));
-		
+
+		persistence.postMetricValue(persistence.getProject().getUid(), String.valueOf(projectQtd));
 	}
 
 }
