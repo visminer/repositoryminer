220e42fe839da9f1adc4e987a168bbe4d33a3e59
DavidPinho <davidpinho08@gmail.com>
2016-03-09 22:05:00 -0300

    [Database] Add find, update and delete methods

--- src/main/java/br/edu/ufba/softvis/visminer/persistence/Database.java
+++ src/main/java/br/edu/ufba/softvis/visminer/persistence/Database.java
@@ -1,11 +1,18 @@
 package br.edu.ufba.softvis.visminer.persistence;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bson.Document;
 
-import br.edu.ufba.softvis.visminer.config.DBConfig;
-
+import com.mongodb.BasicDBObject;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.result.DeleteResult;
+import com.mongodb.client.result.UpdateResult;
+
+import br.edu.ufba.softvis.visminer.config.DBConfig;
 
 /**
  * @author Felipe Gustavo de Souza Gomes (felipegustavo1000@gmail.com)
@@ -41,8 +48,50 @@ public class Database {
 	}
 	
 	public void insert(String collection, Document document) {
-		MongoCollection<Document> coll = getDbCollection(collection);
-		
+		MongoCollection<Document> coll = getDbCollection(collection);		
 		coll.insertOne(document);
+	}
+	
+	public List<Document> findWhere(String collection, BasicDBObject whereClause) {	
+		MongoCursor<Document> cursor = getDbCollection(collection).find(whereClause).iterator();
+		return fromCursorToList(cursor);		
+	}
+	
+	public List<Document> findAll(String collection) {
+		MongoCursor<Document> cursor = getDbCollection(collection).find().iterator();
+		return fromCursorToList(cursor);		
+	}
+	
+	private List<Document> fromCursorToList(MongoCursor<Document> cursor) {
+		List<Document> list = new ArrayList<Document>();
+		
+		while (cursor.hasNext()) {
+			list.add(cursor.next());
+		}		
+		return list;
+	}
+	
+	public DeleteResult deleteOne(String collectionName, BasicDBObject whereClause) {
+		return getDbCollection(collectionName).deleteOne(whereClause);
+	}
+	
+	public DeleteResult deleteMany(String collectionName, BasicDBObject whereClause) {
+		return getDbCollection(collectionName).deleteMany(whereClause);
+	}
+	
+	public DeleteResult deleteAll(String collectionName) {
+		return getDbCollection(collectionName).deleteMany(new BasicDBObject());
+	}	
+	
+	public UpdateResult updateOne(String collectionName, BasicDBObject searchQuery, BasicDBObject newDocument) {
+		return getDbCollection(collectionName).updateOne(searchQuery, newDocument);
+	}
+	
+	public UpdateResult updateMany(String collectionName, BasicDBObject searchQuery, BasicDBObject newDocument) {
+		return getDbCollection(collectionName).updateMany(searchQuery, newDocument);
+	}
+	
+	public UpdateResult updateAll(String collectionName, BasicDBObject newDocument) {
+		return getDbCollection(collectionName).updateMany(new BasicDBObject(), newDocument);
 	}
 }
