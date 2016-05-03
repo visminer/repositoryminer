33a066451d5661c48da398af222bb9f266f4f459
Luis Paulo <luispscarvalho@gmail.com>
2016-05-03 19:10:08 -0300

    Refact AntiPattern >> CodeSmell

--- src/main/java/org/repositoryminer/antipattern/IAntiPattern.java
+++
@@ -1,9 +0,0 @@
-package org.repositoryminer.antipattern;
-
-import org.bson.Document;
-import org.repositoryminer.ast.AST;
-import org.repositoryminer.ast.AbstractTypeDeclaration;
-
-public interface IAntiPattern {
-	public void detect(AbstractTypeDeclaration type, AST ast, Document document);
-}
