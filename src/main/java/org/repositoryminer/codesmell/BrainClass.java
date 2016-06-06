542d61b5853425cf1185baad483e11437bb4ba33
Felipe Gustavo de Souza Gomes <felipegustavo1000@gmail.com>
2016-06-06 00:36:05 -0300

    List of changes:
    1 - Partially merged Pull request #106 with some changes.
    2 - Change of some error messages.
    3 - File path in a diff corrected, now the field "path" in Diff should
    never be null.
    4 - Changes in the way to set
    lines added and removed in changes. Now for REMOVED change type, the
    number of lines deleted will be equal than the number of lines of the
    deleted file. And the same goes on to COPY, except that changes number
    of lines added instead of removed.

--- src/main/java/org/repositoryminer/codesmell/BrainClass.java
+++ src/main/java/org/repositoryminer/codesmell/BrainClass.java
@@ -11,20 +11,32 @@ import org.repositoryminer.metric.TCCMetric;
 import org.repositoryminer.metric.WMCMetric;
 
 public class BrainClass implements ICodeSmell {
+	
+	private int wmcThreshold = 47;
+	private float tccThreshold = 0.5f;
+	private int nbmThreshold = 1;
+	private int slocThreshold = 197;
+	
+	public BrainClass() {}
+	
+	public BrainClass(int wmcThreshold, float tccThreshold, int nbmThreshold, int slocThreshold) {
+		this.wmcThreshold = wmcThreshold;
+		this.tccThreshold = tccThreshold;
+		this.nbmThreshold = nbmThreshold;
+		this.slocThreshold = slocThreshold;
+	}
 
 	@Override
 	public void detect(AbstractTypeDeclaration type, AST ast, Document document) {
 		if (type.getType() == DeclarationType.CLASS_OR_INTERFACE) {
 			TypeDeclaration cls = (TypeDeclaration) type;
-		
 			boolean brainClass = detect(ast, type, cls);
 			document.append("name", new String("Brain Class")).append("value", new Boolean(brainClass));
 		}
 	}
-	
+
 	public boolean detect(AST ast, AbstractTypeDeclaration type, TypeDeclaration cls){
 		boolean brainClass  = false;
-		
 		WMCMetric wmcMetric = new WMCMetric();
 		GodClass godClass = new GodClass();
 		BrainMethod brainMethod = new BrainMethod();
@@ -42,10 +54,10 @@ public class BrainClass implements ICodeSmell {
 				nbm++;
 		}
 		
-		brainClass = isGodClass && (wmc >=47 && tcc < 0.5) && ((nbm > 1 && sloc >= 197) || (nbm == 1 && sloc >= (2*197) && wmc >= (2*47) ));
+		brainClass = isGodClass && (wmc >=wmcThreshold && tcc < tccThreshold) && 
+					((nbm > nbmThreshold && sloc >= slocThreshold) || (nbm == nbmThreshold && sloc >= (2*slocThreshold) && wmc >= (2*wmcThreshold) ));
 		
 		return brainClass;
 	}
-	
 
 }
--- src/main/java/org/repositoryminer/exceptions/ErrorMessage.java
+++ src/main/java/org/repositoryminer/exceptions/ErrorMessage.java
@@ -7,12 +7,12 @@ public enum ErrorMessage {
 	GIT_TAG_LIST_ERROR("GIT - It was impossible to list the tags."),
 	GIT_LOG_COMMIT_ERROR("GIT - It was impossible to list the commits."),
 	GIT_RETRIEVE_CHANGES_ERROR("GIT - It was impossibe to retieve the changes made in one commit."),
-	GIT_CHECKOUT_ERROR("GIT - It was impossible make checkout"), 
+	GIT_CHECKOUT_ERROR("GIT - It was impossible make checkout."), 
 	GIT_RESET_ERROR("GIT - It was impossible to reset the repository."), 
 	GIT_BRANCH_COMMITS_ERROR("GIT - It was impossible to retrieve the commits of a branch."),
 	GIT_TAG_COMMITS_ERROR("GIT - It was impossible to retrieve the commits of a tag."),
 	GIT_RETRIEVE_DATA_ERROR("GIT - It was impossible to retrieve the data from a file."),
-	GIT_REPOSITORY_IOERROR("GIT - It was impossible to analyze the repository."),
+	GIT_REPOSITORY_IOERROR("GIT - It was impossible to analyze the repository, some unexpected IO error has ocurred."),
 	SCM_NOT_FOUND("Not found SCM system."),
 	DUPLICATE_REPOSITORY("Repository already exists in database.");
 	
