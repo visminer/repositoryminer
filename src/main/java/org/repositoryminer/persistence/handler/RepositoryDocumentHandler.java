f12e7c9a718223d52ecd11bbf9785f29f1a83d44
luispscarvalho <luispscarvalho@gmail.com>
2016-10-01 22:37:21 -0300

    + other necessary search routines

--- src/main/java/org/repositoryminer/persistence/handler/RepositoryDocumentHandler.java
+++ src/main/java/org/repositoryminer/persistence/handler/RepositoryDocumentHandler.java
@@ -1,8 +1,12 @@
 package org.repositoryminer.persistence.handler;
 
+import java.util.List;
+
 import org.bson.Document;
 import org.bson.types.ObjectId;
 import org.repositoryminer.persistence.Connection;
+
+import com.mongodb.BasicDBObject;
 
 public class RepositoryDocumentHandler extends DocumentHandler {
 
@@ -12,11 +16,19 @@ public class RepositoryDocumentHandler extends DocumentHandler {
 		super.collection = Connection.getInstance().getCollection(COLLECTION_NAME);
 	}
 
-	public boolean checkIfRepositoryExists(String id) {
+	public boolean checkIfRepositoryExistsById(String id) {
 		Document clause = new Document("_id", new ObjectId(id));
+
 		return collection.count(clause) == 1 ? true : false;
 	}
-	
+
+	public List<Document> findRepositoriesByName(String name) {
+		BasicDBObject whereClause = new BasicDBObject();
+		whereClause.put("name", name);
+
+		return findMany(whereClause);
+	}
+
 	public Document findOnlyContributors(String id) {
 		Document projection = new Document("contributors", 1);
 		return findById(id, projection);
