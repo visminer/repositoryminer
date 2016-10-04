5b480df597a1650ac4aa2795e8b4960ff82b8ba0
Felipe Gustavo de Souza Gomes <felipegustavo1000@gmail.com>
2016-10-03 21:54:57 -0300

    Commit message analyzer for link with issues added.

--- src/main/java/org/repositoryminer/model/IssueReference.java
+++ src/main/java/org/repositoryminer/model/IssueReference.java
@@ -6,18 +6,35 @@ public class IssueReference {
 
 	private IssueCommandType command;
 	private int number;
-	
+
+	public IssueReference() {
+	}
+
+	public IssueReference(IssueCommandType command, int number) {
+		super();
+		this.command = command;
+		this.number = number;
+	}
+
+	public IssueReference(int number) {
+		super();
+		this.number = number;
+	}
+
 	public IssueCommandType getCommand() {
 		return command;
 	}
+
 	public void setCommand(IssueCommandType command) {
 		this.command = command;
 	}
+
 	public int getNumber() {
 		return number;
 	}
+
 	public void setNumber(int number) {
 		this.number = number;
 	}
-	
+
 }
