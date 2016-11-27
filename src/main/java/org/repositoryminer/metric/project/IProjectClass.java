5a177a2338088623e02dbc11b26d0c8818640643
Felipe Gustavo de Souza Gomes <felipegustavo1000@gmail.com>
2016-11-27 17:48:55 -0300

    Added interface for metric calculation in project level.

---
+++ src/main/java/org/repositoryminer/metric/project/IProjectClass.java
@@ -0,0 +1,36 @@
+package org.repositoryminer.metric.project;
+
+import java.util.List;
+
+import org.bson.Document;
+import org.repositoryminer.metric.MetricId;
+import org.repositoryminer.parser.IParser;
+
+/**
+ * This interface defines how to implement metric calculation in project
+ * level.
+ */
+public interface IProjectClass {
+
+	/**
+	 * Activates the metric calculation
+	 * 
+	 * @param parsers
+	 *            the source code parsers
+	 *            ({@link org.repositoryminer.parser.IParser}}).
+	 * @param repositoryPath
+	 *            the repository path.
+	 * @param charset
+	 *            the charset.
+	 * @param document
+	 *            the root of mongodb document in which metrics values must be
+	 *            filled.
+	 */
+	public void calculate(List<IParser> parsers, String repositoryPath, String charset, Document document);
+	
+	/**
+	 * @return The metric ID
+	 */
+	public MetricId getId();
+	
+}
