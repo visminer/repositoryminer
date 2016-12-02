1a0b3e47b9488d06e877ec839eff676e4fa7714a
Felipe Gustavo de Souza Gomes <felipegustavo1000@gmail.com>
2016-12-02 07:21:12 -0300

    Method findByIdColl corrected in CommitDocumentHandler:
    Now the method returns the documents ordered by commit date.

--- src/main/java/org/repositoryminer/persistence/handler/CommitDocumentHandler.java
+++ src/main/java/org/repositoryminer/persistence/handler/CommitDocumentHandler.java
@@ -9,6 +9,7 @@ import org.bson.types.ObjectId;
 import org.repositoryminer.persistence.Connection;
 
 import com.mongodb.BasicDBObject;
+import com.mongodb.client.MongoCursor;
 
 public class CommitDocumentHandler extends DocumentHandler {
 
@@ -19,18 +20,22 @@ public class CommitDocumentHandler extends DocumentHandler {
 	}
 
 	@Override
-	public Document findById(String id, Bson projection){
+	public Document findById(String id, Bson projection) {
 		BasicDBObject whereClause = new BasicDBObject();
 		whereClause.put("_id", id);
-		
+
 		return findOne(whereClause, projection);
 	}
-	
+
 	public List<Document> findByIdColl(String repository, List<String> ids, Bson projection) {
 		List<BasicDBObject> where = new ArrayList<BasicDBObject>(2);
 		where.add(new BasicDBObject("repository", new ObjectId(repository)));
 		where.add(new BasicDBObject("_id", new BasicDBObject("$in", ids)));
-		return findMany(new BasicDBObject("$and", where), projection);
+
+		MongoCursor<Document> cursor = collection.find(new BasicDBObject("$and", where))
+				.sort(new BasicDBObject("commit_date", 1)).projection(projection).iterator();
+
+		return fromCursorToList(cursor);
 	}
-	
+
 }
