a86a3b4dc19751fcf261099a0fb7f36f3e9eecd5
Felipe Gustavo de Souza Gomes <felipegustavo1000@gmail.com>
2016-12-04 16:04:10 -0300

    Find bugs executer classes renamed.

--- src/findbugs/java/org/repositoryminer/findbugs/persistence/FindBugsDocumentHandler.java
+++ src/findbugs/java/org/repositoryminer/findbugs/persistence/FindBugsDocumentHandler.java
@@ -3,11 +3,11 @@ package org.repositoryminer.findbugs.persistence;
 import org.repositoryminer.persistence.Connection;
 import org.repositoryminer.persistence.handler.DocumentHandler;
 
-public class FindBugsPersistence extends DocumentHandler {
+public class FindBugsDocumentHandler extends DocumentHandler {
 
-	private static final String COLLECTION_NAME = "rm_findbugs_analysis";
+	private static final String COLLECTION_NAME = "findbugs_bugs_analysis";
 	
-	public FindBugsPersistence() {
+	public FindBugsDocumentHandler() {
 		super.collection = Connection.getInstance().getCollection(COLLECTION_NAME);
 	}
 	
