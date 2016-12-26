91dfe4042ee66b159e053d18ee443539bd30a4b2
Felipe Gustavo de Souza Gomes <felipegustavo1000@gmail.com>
2016-12-26 03:25:06 -0300

    Removed unnecessary javadoc from AST classes.

--- src/main/java/org/repositoryminer/ast/Document.java
+++ src/main/java/org/repositoryminer/ast/Document.java
@@ -10,77 +10,42 @@ public class Document {
 	private List<ImportDeclaration> imports;
 	private String packageDeclaration;
 
-	/**
-	 * @return the name
-	 */
 	public String getName() {
 		return name;
 	}
 
-	/**
-	 * @param name
-	 *            the name to set
-	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
-	/**
-	 * @return the types
-	 */
 	public List<AbstractTypeDeclaration> getTypes() {
 		return types;
 	}
 
-	/**
-	 * @param types
-	 *            the types to set
-	 */
 	public void setTypes(List<AbstractTypeDeclaration> types) {
 		this.types = types;
 	}
 
-	/**
-	 * @return the methods
-	 */
 	public List<MethodDeclaration> getMethods() {
 		return methods;
 	}
 
-	/**
-	 * @param imports
-	 *            the methods to set
-	 */
 	public void setMethods(List<MethodDeclaration> methods) {
 		this.methods = methods;
 	}
 
-	/**
-	 * @return the imports
-	 */
 	public List<ImportDeclaration> getImports() {
 		return imports;
 	}
 
-	/**
-	 * @param imports
-	 *            the imports to set
-	 */
 	public void setImports(List<ImportDeclaration> imports) {
 		this.imports = imports;
 	}
 	
-	/**
-	 * @return the packageDeclaration
-	 */
 	public String getPackageDeclaration() {
 		return packageDeclaration;
 	}
 
-	/**
-	 * @param packageDeclaration
-	 *            the packageDeclaration to set
-	 */
 	public void setPackageDeclaration(String packageDeclaration) {
 		this.packageDeclaration = packageDeclaration;
 	}
--- src/main/java/org/repositoryminer/ast/TypeDeclaration.java
+++ src/main/java/org/repositoryminer/ast/TypeDeclaration.java
@@ -12,44 +12,26 @@ public class TypeDeclaration extends AbstractTypeDeclaration{
 		setArchetype(Archetype.CLASS_OR_INTERFACE);
 	}
 	
-	/**
-	 * @return the isInterface
-	 */
 	public boolean isInterface() {
 		return isInterface;
 	}
 
-	/**
-	 * @param isInterface the isInterface to set
-	 */
 	public void setInterface(boolean isInterface) {
 		this.isInterface = isInterface;
 	}
 
-	/**
-	 * @return the methods
-	 */
 	public List<MethodDeclaration> getMethods() {
 		return methods;
 	}
 
-	/**
-	 * @param methods the methods to set
-	 */
 	public void setMethods(List<MethodDeclaration> methods) {
 		this.methods = methods;
 	}
 
-	/**
-	 * @return the fields
-	 */
 	public List<FieldDeclaration> getFields() {
 		return fields;
 	}
 
-	/**
-	 * @param fields the fields to set
-	 */
 	public void setFields(List<FieldDeclaration> fields) {
 		this.fields = fields;
 	}
