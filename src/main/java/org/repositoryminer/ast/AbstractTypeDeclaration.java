05567c51b5c55fa119425dae2b872a094a18591d
Felipe Gustavo de Souza Gomes <felipegustavo1000@gmail.com>
2016-12-26 03:25:06 -0300

    Removed unnecessary from AST classes.

--- src/main/java/org/repositoryminer/ast/AbstractTypeDeclaration.java
+++ src/main/java/org/repositoryminer/ast/AbstractTypeDeclaration.java
@@ -9,31 +9,18 @@ public class AbstractTypeDeclaration {
 	private Archetype archetype;
 	private String name;
 
-	/**
-	 * @return the archetype
-	 */
 	public Archetype getArchetype() {
 		return archetype;
 	}
 
-	/**
-	 * @param archetype the archetype to set
-	 */
 	public void setArchetype(Archetype archetype) {
 		this.archetype = archetype;
 	}
 
-	/**
-	 * @return the name
-	 */
 	public String getName() {
 		return name;
 	}
 
-	/**
-	 * @param name
-	 *            the name to set
-	 */
 	public void setName(String name) {
 		this.name = name;
 	}
