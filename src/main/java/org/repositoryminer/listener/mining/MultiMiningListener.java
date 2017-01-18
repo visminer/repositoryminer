933a30cf4eb3525c07237498d53bd34eab4fd0c2
Felipe Gustavo de Souza Gomes <felipegustavo1000@gmail.com>
2017-01-17 23:07:08 -0300

    MultiListeners added.

--- src/main/java/org/repositoryminer/listener/mining/MultiMiningListener.java
+++ src/main/java/org/repositoryminer/listener/mining/MultiMiningListener.java
@@ -1,110 +1,144 @@
 package org.repositoryminer.listener.mining;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.repositoryminer.scm.ReferenceType;
 
 public class MultiMiningListener implements IMiningListener {
 
+	private List<IMiningListener> listeners = new ArrayList<IMiningListener>();
+	
+	public List<IMiningListener> getListeners() {
+		return listeners;
+	}
+
+	public void setListeners(List<IMiningListener> listeners) {
+		this.listeners = listeners;
+	}
+
+	public void addListener(IMiningListener listener) {
+		listeners.add(listener);
+	}
+	
 	@Override
 	public void notifyMiningStart(String repositoryName) {
-		// TODO Auto-generated method stub
-		
+		for (IMiningListener listener : listeners) {
+			listener.notifyMiningStart(repositoryName);
+		}
 	}
 
 	@Override
 	public void notifyMiningEnd(String repositoryName) {
-		// TODO Auto-generated method stub
-		
+		for (IMiningListener listener : listeners) {
+			listener.notifyMiningEnd(repositoryName);
+		}
 	}
 
 	@Override
 	public void notifyReferencesMiningStart(int referencesQtd) {
-		// TODO Auto-generated method stub
-		
+		for (IMiningListener listener : listeners) {
+			listener.notifyReferencesMiningStart(referencesQtd);
+		}
 	}
 
 	@Override
 	public void notifyReferencesMiningProgress(String referenceName, ReferenceType referenceType) {
-		// TODO Auto-generated method stub
-		
+		for (IMiningListener listener : listeners) {
+			listener.notifyReferencesMiningProgress(referenceName, referenceType);
+		}
 	}
 
 	@Override
 	public void notifyReferencesMiningEnd(int referencesQtd) {
-		// TODO Auto-generated method stub
-		
+		for (IMiningListener listener : listeners) {
+			listener.notifyReferencesMiningEnd(referencesQtd);
+		}
 	}
 
 	@Override
 	public void notifyCommitsMiningStart(String referenceName, ReferenceType referenceType, int commitsQtd) {
-		// TODO Auto-generated method stub
-		
+		for (IMiningListener listener : listeners) {
+			listener.notifyCommitsMiningStart(referenceName, referenceType, commitsQtd);
+		}
 	}
 
 	@Override
 	public void notifyCommitsMiningProgress(String referenceName, ReferenceType referenceType, String commit) {
-		// TODO Auto-generated method stub
-		
+		for (IMiningListener listener : listeners) {
+			listener.notifyCommitsMiningProgress(referenceName, referenceType, commit);
+		}
 	}
 
 	@Override
 	public void notifyCommitsMiningEnd(String referenceName, ReferenceType referenceType, int commitsQtd) {
-		// TODO Auto-generated method stub
-		
+		for (IMiningListener listener : listeners) {
+			listener.notifyCommitsMiningEnd(referenceName, referenceType, commitsQtd);
+		}
 	}
 
 	@Override
 	public void notifyWorkingDirectoriesMiningStart(String referenceName, ReferenceType referenceType, int commitsQtd) {
-		// TODO Auto-generated method stub
-		
+		for (IMiningListener listener : listeners) {
+			listener.notifyWorkingDirectoriesMiningStart(referenceName, referenceType, commitsQtd);
+		}
 	}
 
 	@Override
 	public void notifyWorkingDirectoriesMiningProgress(String referenceName, ReferenceType referenceType,
 			String commit) {
-		// TODO Auto-generated method stub
-		
+		for (IMiningListener listener : listeners) {
+			listener.notifyWorkingDirectoriesMiningProgress(referenceName, referenceType, commit);
+		}
 	}
 
 	@Override
 	public void notifyWorkingDirectoriesMiningEnd(String referenceName, ReferenceType referenceType, int commitsQtd) {
-		// TODO Auto-generated method stub
-		
+		for (IMiningListener listener : listeners) {
+			listener.notifyWorkingDirectoriesMiningEnd(referenceName, referenceType, commitsQtd);
+		}
 	}
 
 	@Override
 	public void notifyDirectCodeAnalysisStart(int totalCommits) {
-		// TODO Auto-generated method stub
-		
+		for (IMiningListener listener : listeners) {
+			listener.notifyDirectCodeAnalysisStart(totalCommits);
+		}
 	}
 
 	@Override
 	public void notifyDirectCodeAnalysisProgress(String commit, int index, int totalCommits) {
-		// TODO Auto-generated method stub
-		
+		for (IMiningListener listener : listeners) {
+			listener.notifyDirectCodeAnalysisProgress(commit, index, totalCommits);
+		}
 	}
 
 	@Override
 	public void notifyDirectCodeAnalysisEnd(int totalCommits) {
-		// TODO Auto-generated method stub
-		
+		for (IMiningListener listener : listeners) {
+			listener.notifyDirectCodeAnalysisEnd(totalCommits);
+		}
 	}
 
 	@Override
 	public void notifyIndirectCodeAnalysisStart(int totalSnapshots) {
-		// TODO Auto-generated method stub
-		
+		for (IMiningListener listener : listeners) {
+			listener.notifyIndirectCodeAnalysisStart(totalSnapshots);
+		}
 	}
 
 	@Override
 	public void notifyIndirectCodeAnalysisProgress(String snapshot, int index, int totalSnapshots) {
-		// TODO Auto-generated method stub
-		
+		for (IMiningListener listener : listeners) {
+			listener.notifyIndirectCodeAnalysisProgress(snapshot, index, totalSnapshots);
+		}
 	}
 
 	@Override
 	public void notifyIndirectCodeAnalysisEnd(int totalSnapshots) {
-		// TODO Auto-generated method stub
-		
+		for (IMiningListener listener : listeners) {
+			listener.notifyIndirectCodeAnalysisEnd(totalSnapshots);
+		}
 	}
 
 }
--- src/main/java/org/repositoryminer/listener/servicemining/MultiServiceMiningListener.java
+++ src/main/java/org/repositoryminer/listener/servicemining/MultiServiceMiningListener.java
@@ -1,49 +1,99 @@
 package org.repositoryminer.listener.servicemining;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MultiServiceMiningListener implements IServiceMiningListener {
 
+	private List<IServiceMiningListener> listeners = new ArrayList<IServiceMiningListener>();
+	
+	public List<IServiceMiningListener> getListeners() {
+		return listeners;
+	}
+
+	public void setListeners(List<IServiceMiningListener> listeners) {
+		this.listeners = listeners;
+	}
+
+	public void addListener(IServiceMiningListener listener) {
+		listeners.add(listener);
+	}
+	
 	@Override
 	public void notifyServiceMiningStart(String name) {
+		for (IServiceMiningListener listener : listeners) {
+			listener.notifyServiceMiningStart(name);
+		}
 	}
 
 	@Override
 	public void notifyServiceMiningEnd(String name) {
+		for (IServiceMiningListener listener : listeners) {
+			listener.notifyServiceMiningEnd(name);
+		}
 	}
 
 	@Override
 	public void notifyContributorsMiningStart(int totalContributors) {
+		for (IServiceMiningListener listener : listeners) {
+			listener.notifyContributorsMiningStart(totalContributors);
+		}
 	}
 
 	@Override
 	public void notifyContributorsMiningProgress(String name, int index, int totalContributors) {
+		for (IServiceMiningListener listener : listeners) {
+			listener.notifyContributorsMiningProgress(name, index, totalContributors);
+		}
 	}
 
 	@Override
 	public void notifyContributorsMiningEnd(int totalContributors) {
+		for (IServiceMiningListener listener : listeners) {
+			listener.notifyContributorsMiningEnd(totalContributors);
+		}
 	}
 
 	@Override
 	public void notifyMilestonesMiningStart(int totalMilestones) {
+		for (IServiceMiningListener listener : listeners) {
+			listener.notifyMilestonesMiningStart(totalMilestones);
+		}
 	}
 
 	@Override
 	public void notifyMilestonesMiningProgress(int number, String title, int index, int totalMilestones) {
+		for (IServiceMiningListener listener : listeners) {
+			listener.notifyMilestonesMiningProgress(number, title, index, totalMilestones);
+		}
 	}
 
 	@Override
 	public void notifyMilestonesMiningEnd(int totalMilestones) {
+		for (IServiceMiningListener listener : listeners) {
+			listener.notifyMilestonesMiningEnd(totalMilestones);
+		}
 	}
 
 	@Override
 	public void notifyIssuesMiningStart(int totalIssues) {
+		for (IServiceMiningListener listener : listeners) {
+			listener.notifyIssuesMiningStart(totalIssues);
+		}
 	}
 
 	@Override
 	public void notifyIssuesMiningProgress(int number, String title, int index, int totalIssues) {
+		for (IServiceMiningListener listener : listeners) {
+			listener.notifyIssuesMiningProgress(number, title, index, totalIssues);
+		}
 	}
 
 	@Override
 	public void notifyIssuesMiningEnd(int totalIssues) {
+		for (IServiceMiningListener listener : listeners) {
+			listener.notifyIssuesMiningEnd(totalIssues);
+		}
 	}
 
 }
