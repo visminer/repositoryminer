c6f19e7adafb3a9bdb46193b77ccb1af2f9b647e
Felipe Gustavo de Souza Gomes <felipegustavo1000@gmail.com>
2017-01-21 03:22:21 -0300

    NOPA e NOAM metrics added.

--- src/main/java/org/repositoryminer/codemetric/direct/WOC.java
+++ src/main/java/org/repositoryminer/codemetric/direct/WOC.java
@@ -11,6 +11,8 @@ import org.repositoryminer.codemetric.CodeMetricId;
 
 public class WOC implements IDirectCodeMetric {
 
+	private NOAM noam = new NOAM();
+
 	@Override
 	public Document calculate(AbstractClassDeclaration type, AST ast) {
 		return new Document("metric", CodeMetricId.WOC.toString()).append("value",
@@ -30,10 +32,8 @@ public class WOC implements IDirectCodeMetric {
 		for (MethodDeclaration method : methods) {
 			if (method.getModifiers().contains("public")) {
 				publicMembers++;
-				String name = method.getName();
-				
-				if ((!name.startsWith("is") && !name.startsWith("get") && !name.startsWith("set"))
-						|| isNotAcessor(name, fields)) {
+
+				if (!noam.isAcessor(method.getName(), fields)) {
 					functionalMembers++;
 				}
 			}
@@ -44,32 +44,6 @@ public class WOC implements IDirectCodeMetric {
 		}
 
 		return functionalMembers * 1.0f / publicMembers;
-	}
-
-	private boolean isNotAcessor(String signature, List<FieldDeclaration> fields) {
-		String field;
-
-		if (signature.startsWith("get") || signature.startsWith("set")) {
-			field = signature.substring(3, signature.indexOf('('));
-		} else {
-			field = signature.substring(2, signature.indexOf('('));
-		}
-
-		if (field.length() == 0) {
-			return true;
-		}
-
-		char c[] = field.toCharArray();
-		c[0] = Character.toLowerCase(c[0]);
-		String field2 = new String(c);
-
-		for (FieldDeclaration fd : fields) {
-			if (fd.getName().equals(field) || fd.getName().equals(field2)) {
-				return false;
-			}
-		}
-
-		return true;
 	}
 
 	@Override
